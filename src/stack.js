b21b90adb71e5e9a086cca3164b6c05ba24a928f
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-10-18 01:28:43 +0500

    feat: stack solution

--- src/stack.js
+++ src/stack.js
@@ -12,20 +12,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
+
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.answer = [];
+  }
+  
+  push(x) {
+    this.answer.push(x);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.answer.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.answer[this.answer.length - 1]
   }
 }
 
